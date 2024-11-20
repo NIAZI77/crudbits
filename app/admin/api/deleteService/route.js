7660886be6c21cfdd3bc2ce2153676419ec407e4
NIAZI77 <147417302+NIAZI77@users.noreply.github.com>
2024-11-20 08:09:15 +0500

    Update route.js

--- app/admin/api/deleteService/route.js
+++ app/admin/api/deleteService/route.js
@@ -1,5 +1,5 @@
 import { MongoClient, ObjectId } from 'mongodb';
-
+export const revalidate = 60
 export async function DELETE(req) {
     const uri = process.env.MONGODB_URI;
     const client = new MongoClient(uri);
